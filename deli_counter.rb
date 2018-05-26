bff57998ce6f672b475ca2533aeed816ad4f8c8d
KoichiSakamoto <isaac.fiore@mail.mcgill.ca>
2018-05-26 17:35:19 -0400

    implemented deli counter

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,28 @@
 # Write your code here.
+
+def line(deliLine)
+  if deliLine.length == 0
+    puts "The line is currently empty."
+  else
+    returnString = "The line is currently: "
+    counter = 1
+    deliLine.each do |person|
+      returnString.concat("#{counter}. #{person} ")
+      counter += 1
+    end
+    puts returnString.chomp(" ")
+  end
+end
+
+def take_a_number(deliLine, name)
+  deliLine.push(name)
+  puts "Welcome, #{name}. You are number #{deliLine.length} in line."
+end
+
+def now_serving(deliLine)
+  if deliLine.length == 0
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{deliLine.shift}."
+  end
+end
