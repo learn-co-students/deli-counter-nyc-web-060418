b3bfe04350963d0ac8c20e1e993443e140fe5fd9
Giles Bonner <gjbonner@gmail.com>
2018-05-27 20:32:08 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,3 +12,25 @@ def line(current_line)
   end
 end
 
+def take_a_number(katz_deli, name)
+  if katz_deli.length == 0
+    puts "Welcome, #{name}. You are number 1 in line."
+  else
+  line_length = katz_deli.length + 1
+    name.each do |each_name|
+    puts "Welcome, #{each_name}. You are number #{line_length} in line."
+    katz_deli.push(each_name)
+   end
+  end
+  return katz_deli
+end
+
+def now_serving(katz_deli)
+  if katz_deli.length == 0
+    puts "There is nobody waiting to be served!"
+  else
+   puts "Currently serving #{katz_deli[0]}."
+   katz_deli.shift
+  end
+  return katz_deli
+end
