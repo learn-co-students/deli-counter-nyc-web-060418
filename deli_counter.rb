f8571e1fc81a5c034780ff9c10800c46e378275e
Giles Bonner <gjbonner@gmail.com>
2018-05-27 20:05:51 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,13 @@
 # Write your code here.
+katz_deli = []
+
+def line(in_line)
+  place_in_line = []
+  if in_line.length == 0
+    puts "The line is currently empty."
+  else
+    in_line.each.with_index(1) do |person, index|
+      place_in_line.push("The line is currently: #{index}. #{person}")
+    end
+    end
+end
